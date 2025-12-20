604ffee5f8ad6b30c61287f903cd813883e67e97
Lihn2254 <erick.davidhf@gmail.com>
2025-12-19 23:02:14 -0700

    Created an scheduled service on backend to mark tickets as expired

--- backend/src/main/java/com/tickethub/repositories/TicketRepository.java
+++ backend/src/main/java/com/tickethub/repositories/TicketRepository.java
@@ -1,11 +1,27 @@
 package com.tickethub.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.tickethub.domain.Ticket;
 
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
+import jakarta.transaction.Transactional;
+
+public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+    @Modifying // Required for UPDATE/DELETE queries
+    @Transactional // Required to execute changes
+    @Query("""
+        UPDATE Ticket t 
+        SET t.status = 3 
+        WHERE t.status = 1 
+        AND t.event.startTime < :cutoffTime
+    """)
+    int expireOldTickets(@Param("cutoffTime") Date cutoffTime);
+    
     List<Ticket> findByOrder_Client_Id(Long clientId);
 }
